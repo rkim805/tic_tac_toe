2ef2b9617b52760b9eea3cfbd2c7920a4b8f45a2
Ryan Kim <rkim805@gmail.com>
2021-10-08 15:45:33 -0700

    Add function to click on tiles to display symbol
    This function, addTileListener, will add a listener to the window.
    When an event bubbles up and confirms that the click was on a tile,
    the event will update the board state, and display the symbol on
    the board.

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,13 +1,63 @@
 "use strict"
-const playerFactory = () => {
+const playerFactory = (symbol) => {
   const score = 0;
-  return {};
+  return {score, symbol};
 };
 
 
-const displayController = () => ({ 
+const displayController = (() => { 
+
+  const displayTile = (tile, symbol) => {
+    tile.textContent = symbol;
+  };
+
+  const resetDisplay = () => {
+    console.log("test");
+  };
+ 
+  return {
+    displayTile
+  }
 })();
 
-const gameBoard = () => ({
+const gameBoard = (() => {
+  const board = [[...Array(3)],[...Array(3)],[...Array(3)]];
 
+  const setBoardTile = (row, col, symbol) => {
+    board[row][col] = symbol;
+  };
+
+  const getBoardTile = (row, col) => {
+    return board[row][col];
+  };
+
+  const addTileListener = (symbol) => {
+    window.addEventListener("click", (event) => {
+      if(event.target.className === "tile") {
+        let row = event.target.getAttribute("data-row");
+        let col = event.target.getAttribute("data-col");
+        setBoardTile(row, col, symbol);
+        displayController.displayTile(event.target, symbol);
+      }
+    });
+  }
+
+
+  return {
+    //addTileListeners,
+    addTileListener,
+    getBoardTile
+  };
 })();
+
+const game = (() => {
+  let playerTurn;
+  const init = () => {
+    gameBoard.addTileListener("X");
+  }
+  return {
+    init
+  }
+})();
+
+window.onload = game.init();
