d069f46568669684e129d21bdf8287448a5c8391
Ryan Kim <rkim805@gmail.com>
2021-10-08 20:53:46 -0700

    Encapsulate previous modules in one outer module
    Misc changes:
    -- Add comments to helper win-condition checks
    -- remove addTileListener -- function body was short and understandable
       enough to remove function and replace with function body where called

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,217 +1,245 @@
 "use strict"
-const playerFactory = (symbol) => {
-  const score = 0;
-  return { score, symbol };
-};
-
-
-const displayController = (() => {
-
-  const displayTile = (tile, symbol) => {
-    tile.textContent = symbol;
+const ticTacToe = (() => {  
+  const playerFactory = (symbol) => {
+    const score = 0;
+    return { score, symbol };
   };
 
-  const displayWinMessage = (symbol) => {
-    const display = document.querySelector("#game-status");
-    display.textContent = `Congrats to Player ${symbol} on winning!!`;
-  };
 
-  const displayTieMessage = () => {
-    const display = document.querySelector("#game-status");
-    display.textContent = "Tie!";
-  }
+  const displayController = (() => {
 
-  const resetDisplay = () => {
-    console.log("test");
-  };
+    const displayTile = (tile, symbol) => {
+      tile.textContent = symbol;
+    };
 
-  return {
-    displayTile,
-    displayWinMessage,
-    displayTieMessage
-  }
-})();
+    const displayWinMessage = (symbol) => {
+      const display = document.querySelector("#game-status");
+      display.textContent = `Congrats to Player ${symbol} on winning!!`;
+    };
 
-const gameBoard = (() => {
-  const ROW_SIZE = 3;
-  const board = [[...Array(ROW_SIZE)], [...Array(ROW_SIZE)], 
-    [...Array(ROW_SIZE)]];
+    const displayTieMessage = () => {
+      const display = document.querySelector("#game-status");
+      display.textContent = "Tie!";
+    }
 
-  const setBoardTile = (row, col, symbol) => {
-    board[row][col] = symbol;
-  };
+    const resetDisplay = () => {
+      console.log("test");
+    };
 
-  const getBoardTile = (row, col) => {
-    return board[row][col];
-  };
+    return {
+      displayTile,
+      displayWinMessage,
+      displayTieMessage
+    }
+  })();
 
-  const isBoardFull = () => {
-    for(let row = 0; row < board.length; row++) {
-      for(let col = 0; col < board[row].length; col++) {
-        if(board[row][col] == undefined) {
-          return false;
+  const gameBoard = (() => {
+    const ROW_SIZE = 3;
+    const board = [[...Array(ROW_SIZE)], [...Array(ROW_SIZE)], 
+      [...Array(ROW_SIZE)]];
+
+    const setBoardTile = (row, col, symbol) => {
+      board[row][col] = symbol;
+    };
+
+    const getBoardTile = (row, col) => {
+      return board[row][col];
+    };
+
+    const isBoardFull = () => {
+      for(let row = 0; row < board.length; row++) {
+        for(let col = 0; col < board[row].length; col++) {
+          if(board[row][col] == undefined) {
+            return false;
+          }
         }
       }
+      return true;
     }
-    return true;
-  }
 
-  return {
-    setBoardTile,
-    getBoardTile,
-    isBoardFull
-  };
-})();
+    return {
+      setBoardTile,
+      getBoardTile,
+      isBoardFull
+    };
+  })();
 
-const gameLogic = (() => {
-  const ROW_SIZE = 3;
-  const COL_SIZE = 3;
+  const gameLogic = (() => {
+    const ROW_SIZE = 3;
+    const COL_SIZE = 3;
 
-  const players = [];
-  let currentTurnIndex;
-  let gameOver = false;
+    const players = [];
+    let currentTurnIndex;
+    let gameOver = false;
 
-  const init = () => {
-    players.push(playerFactory("X"), playerFactory("O"));
-    currentTurnIndex = 0;
-    addTileListener();
-  }
+    const init = () => {
+      players.push(playerFactory("X"), playerFactory("O"));
+      currentTurnIndex = 0;
+      window.addEventListener("click", handleTileClick);
+    }
 
-  const _getCurrentTurnSymbol = () => {
-    return players[currentTurnIndex].symbol;
-  }
+    const _getCurrentTurnSymbol = () => {
+      return players[currentTurnIndex].symbol;
+    }
 
-  const _toggleTurnIndex = () => {
-    return currentTurnIndex === 0 ? 1 : 0;
-  }
-  
-  /**
-   * addTileListener()
-   * 
-   * Uses event bubbling to listen to all clicks -- callback runs full
-   * logic when clicks are of .tile class. Uses less listeners 
-   * than binding to each individual tile.
-   */
-  const addTileListener = () => {
-    window.addEventListener("click", handleTileClick);
-  }
+    const _toggleTurnIndex = () => {
+      return currentTurnIndex === 0 ? 1 : 0;
+    }
+    
+    /**
+     * handleTileClick() -
+     * Handler when a .tile class element is clicked.
+     * First checks to see if element clicked is a tile, if tile hasn't been
+     * clicked, or if the game is over.
+     * 
+     * Then checks to see if game was won or tied with this click.
+     * 
+     * @param {Object} event - Click event bubbled to window 
+     */
+    const handleTileClick = (event) => {
+      if (event.target.className === "tile" && event.target.textContent == "" &&
+      !gameOver) {
+        const symbol = _getCurrentTurnSymbol();
+        const row = event.target.getAttribute("data-row");
+        const col = event.target.getAttribute("data-col");
 
-  /**
-   * handleTileClick() -
-   * Handler when a .tile class element is clicked.
-   * First checks to see if element clicked is a tile, if tile hasn't been
-   * clicked, or if the game is over.
-   * 
-   * Then checks to see if game was won or tied with this click.
-   * 
-   * @param {Object} event - Click event bubbled to window 
-   */
-  const handleTileClick = (event) => {
-    if (event.target.className === "tile" && event.target.textContent == "" &&
-    !gameOver) {
-      const symbol = _getCurrentTurnSymbol();
-      const row = event.target.getAttribute("data-row");
-      const col = event.target.getAttribute("data-col");
-
-      gameBoard.setBoardTile(row, col, symbol);
-      displayController.displayTile(event.target, symbol);
-      let checkResult = _checkForWin();
-      if(checkResult) {
-        displayController.displayWinMessage(checkResult);
-        gameOver = true;
+        gameBoard.setBoardTile(row, col, symbol);
+        displayController.displayTile(event.target, symbol);
+        let checkResult = _checkForWin();
+        if(checkResult) {
+          displayController.displayWinMessage(checkResult);
+          gameOver = true;
+        }
+        else if(gameBoard.isBoardFull()) {
+          displayController.displayTieMessage();
+          gameOver = true;
+        }
+        currentTurnIndex = _toggleTurnIndex();
       }
-      else if(gameBoard.isBoardFull()) {
-        displayController.displayTieMessage();
-        gameOver = true;
-      }
-      currentTurnIndex = _toggleTurnIndex();
     }
-  }
 
-  const _checkForWin = () => {
-    const rowResult = _checkRowsForWin();
-    if(rowResult) {
-      return rowResult;
+    const _checkForWin = () => {
+      const rowResult = _checkRowsForWin();
+      if(rowResult) {
+        return rowResult;
+      }
+      const colResult = _checkColsForWin();
+      if(colResult) {
+        return colResult;
+      }
+      const declineDiagResult = _checkDeclineDiagonal();
+      if(declineDiagResult) {
+        return declineDiagResult;
+      }
+      const inclineDiagResult = _checkInclineDiagonal();
+      if(inclineDiagResult) {
+        return inclineDiagResult;
+      }
+      else {
+        return false;
+      }
     }
-    const colResult = _checkColsForWin();
-    if(colResult) {
-      return colResult;
+
+    const _checkIfSetWins = (set) => {
+      if(set.includes(undefined)) {
+        return false;
+      }
+      //check if every element in a checked array is the same
+      return set.every(elem => elem === set[0]);
     }
-    const declineDiagResult = _checkDeclineDiagonal();
-    if(declineDiagResult) {
-      return declineDiagResult;
-    }
-    const inclineDiagResult = _checkInclineDiagonal();
-    if(inclineDiagResult) {
-      return inclineDiagResult;
-    }
-    else {
+
+    /**_checkRowsForWin()
+     * Function that iterates through each row of the board,
+     * and sees if there are 3 of the same symbol in a row.
+     * 
+     * @return symbol -- symbol with 3 in a row
+     *         false  -- if no rows with 3 in a row
+     */
+    const _checkRowsForWin = () => {
+      const checkedSet = Array(3);
+      for(let row = 0; row < ROW_SIZE; row++) {
+        for(let col = 0; col < COL_SIZE; col++) {
+          checkedSet[col] = gameBoard.getBoardTile(row, col);
+        }
+        if(_checkIfSetWins(checkedSet)) {
+          return checkedSet[0];
+        }
+      }
       return false;
     }
-  }
 
-  //funcion to check if every element in a checked array is the same
-  const _checkIfSetWins = (set) => {
-    if(set.includes(undefined)) {
+    /**
+     * _checkColsForWin()
+     * Function that iterates through each column of the board,
+     * and sees if there are 3 of the same symbol in a row.
+     * 
+     * @return symbol -- symbol with 3 in a row
+     *         false  -- if no columns with 3 in a row
+     */
+    const _checkColsForWin = () => {
+      const checkedSet = Array(3);
+      for(let col = 0; col < ROW_SIZE; col++) {
+        for(let row = 0; row < COL_SIZE; row++) {
+          checkedSet[row] = gameBoard.getBoardTile(row, col);
+        }
+        if(_checkIfSetWins(checkedSet)) {
+          return checkedSet[0];
+        }
+      }
       return false;
     }
-    return set.every(elem => elem === set[0]);
-  }
 
-  const _checkRowsForWin = () => {
-    const checkedSet = Array(3);
-    for(let row = 0; row < ROW_SIZE; row++) {
-      for(let col = 0; col < COL_SIZE; col++) {
+    /**
+     * _checkDeclineDiagonal
+     * Function that checks the diagonal that goes from the top left of the
+     * board to the bottom right, to see if it is 3 of the same symbols in a
+     * row.
+     * 
+     * @return symbol -- symbol with 3 in a row
+     *         false  -- if no columns with 3 in a row
+     */
+    const _checkDeclineDiagonal = () => {
+      const checkedSet = Array(3);
+      for(let diag = 0; diag < ROW_SIZE; diag++) {
+        checkedSet[diag] = gameBoard.getBoardTile(diag, diag);
+      }      
+      if(_checkIfSetWins(checkedSet)) {
+        return checkedSet[0];
+      }
+      return false;
+    }
+    
+    /**
+     * _checkDeclineDiagonal
+     * Function that checks the diagonal that goes from the bottom left of the
+     * board to the top right, to see if it is 3 of the same symbols in a
+     * row.
+     * 
+     * @return symbol -- symbol with 3 in a row
+     *         false  -- if no columns with 3 in a row
+     */
+    const _checkInclineDiagonal = () => {
+      let col = 0;
+      const checkedSet = Array(3);
+      for(let row = ROW_SIZE - 1; row >= 0; row--) {
         checkedSet[col] = gameBoard.getBoardTile(row, col);
+        col++;
       }
       if(_checkIfSetWins(checkedSet)) {
         return checkedSet[0];
       }
+      return false;
     }
-    return false;
-  }
 
-  const _checkColsForWin = () => {
-    const checkedSet = Array(3);
-    for(let col = 0; col < ROW_SIZE; col++) {
-      for(let row = 0; row < COL_SIZE; row++) {
-        checkedSet[row] = gameBoard.getBoardTile(row, col);
-      }
-      if(_checkIfSetWins(checkedSet)) {
-        return checkedSet[0];
-      }
+    return {
+      init
     }
-    return false;
-  }
-
-  const _checkDeclineDiagonal = () => {
-    const checkedSet = Array(3);
-    for(let diag = 0; diag < ROW_SIZE; diag++) {
-      checkedSet[diag] = gameBoard.getBoardTile(diag, diag);
-    }      
-    if(_checkIfSetWins(checkedSet)) {
-      return checkedSet[0];
-    }
-    return false;
-  }
-
-  
-  const _checkInclineDiagonal = () => {
-    let col = 0;
-    const checkedSet = Array(3);
-    for(let row = ROW_SIZE - 1; row >= 0; row--) {
-      checkedSet[col] = gameBoard.getBoardTile(row, col);
-      col++;
-    }
-    if(_checkIfSetWins(checkedSet)) {
-      return checkedSet[0];
-    }
-    return false;
-  }
+  })();
 
   return {
-    init
+    gameLogic
   }
 })();
 
-window.onload = gameLogic.init();
+
+window.onload = ticTacToe.gameLogic.init();
