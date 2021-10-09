7c8b78885f24e88df46d42266578fd36f5e34863
Ryan Kim <rkim805@gmail.com>
2021-10-08 17:21:41 -0700

    Add function for win condition logic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -21,7 +21,9 @@ const displayController = (() => {
 })();
 
 const gameBoard = (() => {
-  const board = [[...Array(3)], [...Array(3)], [...Array(3)]];
+  const ROW_SIZE = 3;
+  const board = [[...Array(ROW_SIZE)], [...Array(ROW_SIZE)], 
+    [...Array(ROW_SIZE)]];
 
   const setBoardTile = (row, col, symbol) => {
     board[row][col] = symbol;
@@ -31,16 +33,32 @@ const gameBoard = (() => {
     return board[row][col];
   };
 
+  const isBoardFull = () => {
+    for(let row = 0; row < board.length; row++) {
+      for(let col = 0; col < board[row].length; col++) {
+        if(board[row][col] == "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   return {
     setBoardTile,
-    getBoardTile
+    getBoardTile,
+    isBoardFull
   };
 })();
 
 const gameLogic = (() => {
+  const ROW_SIZE = 3;
+  const COL_SIZE = 3;
+
   const players = [];
   let currentTurnIndex;
   let gameOver = false;
+
   const init = () => {
     players.push(playerFactory("X"), playerFactory("O"));
     currentTurnIndex = 0;
@@ -74,10 +92,84 @@ const gameLogic = (() => {
       const symbol = _getCurrentTurnSymbol();
       const row = event.target.getAttribute("data-row");
       const col = event.target.getAttribute("data-col");
+
       gameBoard.setBoardTile(row, col, symbol);
       displayController.displayTile(event.target, symbol);
       currentTurnIndex = _toggleTurnIndex();
     }
+  }
+
+  const _checkForWin = () => {
+    const rowResult = _checkRowsForWin();
+    if(rowResult.won) {
+      return rowResult;
+    }
+    const colResult = _checkColsForWin();
+    if(columnResult.won) {
+      return columnResult;
+    }
+    const declineDiagResult = _checkDeclineDiagonal();
+    if(declineDiagResult.won) {
+      return declineDiagResult;
+    }
+    const inclineDiagResult = _checkInclineDiagonal();
+    if(inclineDiagResult.won) {
+      return inclineDiagResult
+    }
+  }
+
+
+  // fuction that uses Javascript Set's uniqueness property to check if
+  // set contains only one symbol.
+  const _checkIfSetWins = (setOf3) => {
+    return (new Set(arr)).size === 1;
+  }
+
+  const _checkRowsForWin = () => {
+    for(let row = 0; row < ROW_SIZE; row++) {
+      for(let col = 0; col < COL_SIZE; col++) {
+        checkedSet[col] = gameBoard.getBoardTile(row, col);
+      }
+      if(_checkIfSetWins(checkedSet)) {
+        return {won: true, symbol: getBoardTile(row, col)}
+      }
+    }
+    return {won: false};
+  }
+
+  const _checkColsForWin = () => {
+    for(let col = 0; col < ROW_SIZE; col++) {
+      for(let row = 0; row < COL_SIZE; row++) {
+        checkedSet[row] = gameBoard.getBoardTile(row, col);
+      }
+      if(_checkIfSetWins(checkedSet)) {
+        return {won: true, symbol: getBoardTile(row, col)}
+      }
+    }
+    return {won: false};
+  }
+
+  const _checkDeclineDiagonal = () => {
+    for(let diag = 0; diag < ROW_SIZE; diag++) {
+      checkedSet[diag] = gameBoard.getBoardTile(row, col);
+    }      
+    if(_checkIfSetWins(checkedSet)) {
+      return {won: true, symbol: getBoardTile(row, col)}
+    }
+    return {won: false};
+  }
+
+  
+  const _checkInclineDiagonal = () => {
+    let col = 0;
+    for(let row = ROW_SIZE - 1; row >= 0; row--) {
+      checkedSet[col] = gameBoard.getBoardTile(row, col);
+      col++;
+    }
+    if(_checkIfSetWins(checkedSet)) {
+      return {won: true, symbol: getBoardTile(row, col)}
+    }
+    return {won: false};
   }
 
   return {
