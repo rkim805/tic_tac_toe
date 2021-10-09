afcbf3db57c5e7ba9a669cad00aea82fde3535c4
Ryan Kim <rkim805@gmail.com>
2021-10-08 20:35:21 -0700

    Add logic for ties, display message for wins/ties
    Helper functions for _checkForWin() have simplified return values:
    returns false if win condition not met, returns symbol for the winning
    function.
    
    displayWinMessage uses the symbol returned from helper functions to
    display which player wins. displayTieMessage was the function added to
    display a tie message.

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -11,12 +11,24 @@ const displayController = (() => {
     tile.textContent = symbol;
   };
 
+  const displayWinMessage = (symbol) => {
+    const display = document.querySelector("#game-status");
+    display.textContent = `Congrats to Player ${symbol} on winning!!`;
+  };
+
+  const displayTieMessage = () => {
+    const display = document.querySelector("#game-status");
+    display.textContent = "Tie!";
+  }
+
   const resetDisplay = () => {
     console.log("test");
   };
 
   return {
-    displayTile
+    displayTile,
+    displayWinMessage,
+    displayTieMessage
   }
 })();
 
@@ -36,7 +48,7 @@ const gameBoard = (() => {
   const isBoardFull = () => {
     for(let row = 0; row < board.length; row++) {
       for(let col = 0; col < board[row].length; col++) {
-        if(board[row][col] == "") {
+        if(board[row][col] == undefined) {
           return false;
         }
       }
@@ -76,16 +88,24 @@ const gameLogic = (() => {
   /**
    * addTileListener()
    * 
-   * Uses event bubbling to listen to all clicks -- handles when clicks are
-   * of .tile class. Uses less listeners than binding to each individual tile.
-   * 
-   * Function will then update the board array, display the symbol in the
-   * clicked tile, and toggle the turn to be the other player's turn.
+   * Uses event bubbling to listen to all clicks -- callback runs full
+   * logic when clicks are of .tile class. Uses less listeners 
+   * than binding to each individual tile.
    */
   const addTileListener = () => {
     window.addEventListener("click", handleTileClick);
   }
 
+  /**
+   * handleTileClick() -
+   * Handler when a .tile class element is clicked.
+   * First checks to see if element clicked is a tile, if tile hasn't been
+   * clicked, or if the game is over.
+   * 
+   * Then checks to see if game was won or tied with this click.
+   * 
+   * @param {Object} event - Click event bubbled to window 
+   */
   const handleTileClick = (event) => {
     if (event.target.className === "tile" && event.target.textContent == "" &&
     !gameOver) {
@@ -95,81 +115,98 @@ const gameLogic = (() => {
 
       gameBoard.setBoardTile(row, col, symbol);
       displayController.displayTile(event.target, symbol);
+      let checkResult = _checkForWin();
+      if(checkResult) {
+        displayController.displayWinMessage(checkResult);
+        gameOver = true;
+      }
+      else if(gameBoard.isBoardFull()) {
+        displayController.displayTieMessage();
+        gameOver = true;
+      }
       currentTurnIndex = _toggleTurnIndex();
     }
   }
 
   const _checkForWin = () => {
     const rowResult = _checkRowsForWin();
-    if(rowResult.won) {
+    if(rowResult) {
       return rowResult;
     }
     const colResult = _checkColsForWin();
-    if(columnResult.won) {
-      return columnResult;
+    if(colResult) {
+      return colResult;
     }
     const declineDiagResult = _checkDeclineDiagonal();
-    if(declineDiagResult.won) {
+    if(declineDiagResult) {
       return declineDiagResult;
     }
     const inclineDiagResult = _checkInclineDiagonal();
-    if(inclineDiagResult.won) {
-      return inclineDiagResult
+    if(inclineDiagResult) {
+      return inclineDiagResult;
+    }
+    else {
+      return false;
     }
   }
 
-
-  // fuction that uses Javascript Set's uniqueness property to check if
-  // set contains only one symbol.
-  const _checkIfSetWins = (setOf3) => {
-    return (new Set(arr)).size === 1;
+  //funcion to check if every element in a checked array is the same
+  const _checkIfSetWins = (set) => {
+    if(set.includes(undefined)) {
+      return false;
+    }
+    return set.every(elem => elem === set[0]);
   }
 
   const _checkRowsForWin = () => {
+    const checkedSet = Array(3);
     for(let row = 0; row < ROW_SIZE; row++) {
       for(let col = 0; col < COL_SIZE; col++) {
         checkedSet[col] = gameBoard.getBoardTile(row, col);
       }
       if(_checkIfSetWins(checkedSet)) {
-        return {won: true, symbol: getBoardTile(row, col)}
+        return checkedSet[0];
       }
     }
-    return {won: false};
+    return false;
   }
 
   const _checkColsForWin = () => {
+    const checkedSet = Array(3);
     for(let col = 0; col < ROW_SIZE; col++) {
       for(let row = 0; row < COL_SIZE; row++) {
         checkedSet[row] = gameBoard.getBoardTile(row, col);
       }
       if(_checkIfSetWins(checkedSet)) {
-        return {won: true, symbol: getBoardTile(row, col)}
+        return checkedSet[0];
       }
     }
-    return {won: false};
+    return false;
   }
 
   const _checkDeclineDiagonal = () => {
+    const checkedSet = Array(3);
     for(let diag = 0; diag < ROW_SIZE; diag++) {
-      checkedSet[diag] = gameBoard.getBoardTile(row, col);
+      checkedSet[diag] = gameBoard.getBoardTile(diag, diag);
     }      
     if(_checkIfSetWins(checkedSet)) {
-      return {won: true, symbol: getBoardTile(row, col)}
+      return checkedSet[0];
     }
-    return {won: false};
+    return false;
   }
 
   
   const _checkInclineDiagonal = () => {
     let col = 0;
+    const checkedSet = Array(3);
     for(let row = ROW_SIZE - 1; row >= 0; row--) {
       checkedSet[col] = gameBoard.getBoardTile(row, col);
       col++;
     }
     if(_checkIfSetWins(checkedSet)) {
-      return {won: true, symbol: getBoardTile(row, col)}
+      return checkedSet[0];
     }
-    return {won: false};
+    return false;
   }
 
   return {
